175944829093be563927dca031dcfbccdb12b3db
Camila Fernandez <camila.fernandez@utec.edu.pe>
2019-05-08 11:33:27 -0500

    created static_hash branch

---
+++ hash.h
@@ -0,0 +1,13 @@
+#ifndef HASH_H
+#define HASH_H
+
+class Hash{
+private:
+    int num_buckets;
+    int fd;
+
+public:
+    Hash(int N, int fd)
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "hash.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
