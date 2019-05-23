7bf5301ecc9b2ea9fd43a93b6c0ab631abaa1b76
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2019-05-22 23:57:51 -0500

    Pending Revision

--- hash.h
+++ hash.h
@@ -16,7 +16,7 @@ using namespace std;
 
 class Hash{
 private:
-    int HashTable_size = 0;
+    int HashTable_size;
     int fd;
     int num_buckets = 0;
     mutex m;
@@ -24,7 +24,7 @@ private:
 
 public:
     Hash(int N, int fd_input, string filename){
-        if(!is_file_exist("index.txt")){
+        if(!is_file_exist("hash.txt")){
             HashTable_size = N;
             hash_table = new Bucket[N];
             fd = fd_input;
@@ -43,7 +43,6 @@ public:
             input_file.open(filename);
 
             string tmp;
-            //stringstream line;
             if(input_file.is_open()){
                 while(getline(input_file,tmp)){
                     insert_hash(tmp);
@@ -51,68 +50,50 @@ public:
             }
             input_file.close();
 
-            //Creation of index file
-            ofstream index_file;
-            index_file.open("index.txt");
+	    ofstream hash_file;
+	    hash_file.open("hash.txt");
+	    if(hash_file.is_open()){
+		    hash_file << to_string(HashTable_size) << endl;
+		    hash_file << to_string(num_buckets) << endl;
+		    hash_file << to_string(fd) << endl;
+	    }
+	    hash_file.close();
 
-            if(index_file.is_open()){
-                index_file << to_string(num_buckets) << endl;
-                index_file << to_string(fd) << endl;
-                for(int i = 0; i < HashTable_size; i++){
-                    index_file << (hash_table[i]).bucket_name;
+            //Creation of index files
+	    for (int i = 0; i < HashTable_size; i++){
+            	ofstream index_file;
+	        index_file.open(to_string(i).append("index.txt"));
+        	if(index_file.is_open()){
+                    index_file << (hash_table[i]).bucket_name << endl;
                     Bucket *tmp = (hash_table[i]).overflow_bucket;
                     while(tmp){
-                        index_file << "," << tmp->bucket_name;
+                        index_file << tmp->bucket_name << endl;
                         tmp = tmp->overflow_bucket;
                     }
-                    index_file << endl;
-                }
-            }
-
-            index_file.close();
+            	}
+            	index_file.close();
+	    }
         }
+	else
+	    Hash("hash.txt");
 
     }
 
-    Hash(string index){
-        ifstream index_file;
-        index_file.open(index);
-        if(index_file.is_open()){
+    Hash(string hash){
+        ifstream hash_file;
+        hash_file.open(hash);
+        if(hash_file.is_open()){
             string tmp;
-            getline(index_file, tmp);
+            getline(hash_file, tmp);
+            HashTable_size = stoi(tmp);
+            getline(hash_file, tmp);
             num_buckets = stoi(tmp);
-            getline(index_file, tmp);
+            getline(hash_file, tmp);
             fd = stoi(tmp);
-            while (getline(index_file, tmp)){
-                HashTable_size++;
-            }
         }
+	hash_file.close();
     }
 
-    ~Hash(){
-        string tmp;
-        vector <string> tmp_vec;
-
-        ifstream old_index;
-        old_index.open("index.txt");
-        if(old_index.is_open()){
-            getline(old_index,tmp);
-            while(getline(old_index, tmp)){
-                tmp_vec.push_back(tmp);
-            }
-        }  
-        old_index.close();
-        
-        ofstream new_index;
-        new_index.open("index.txt");
-        if(new_index.is_open()){
-            new_index << to_string(num_buckets) << endl;
-            for(auto& it: tmp_vec){
-                new_index << it << endl;
-            }
-        }
-
-    }
 
     int hash_function(int key){
         return key % HashTable_size;
@@ -160,110 +141,95 @@ public:
         int bucket_id = hash_function(stoi(input.substr(0, input.find(","))));
         m.lock();
 
-        ifstream input_file;
-        input_file.open("index.txt");
-        string tmp;
-        string last_bucket;
-        if(input_file.is_open()){
-            for(int i=0; i<=bucket_id; i++){
-                getline(input_file,tmp);
-            }
-            size_t found = tmp.find_last_of(",");
-
-            last_bucket = tmp.substr(found+1);
+        ifstream bucket_table;
+        bucket_table.open(to_string(bucket_id).append("index.txt"));
+        string bucket, tmp;
+        if(bucket_table.is_open()){
+	    cout << "buscando ultimo bucket\n"; 
+	    while(getline(bucket_table, tmp)){
+	    	bucket = tmp;
+	    }
+	    cout << "ultimo bucket: " << bucket << endl;
         }
-        input_file.close();
+        bucket_table.close();
 
         ifstream bucket_file;
         int bucket_counter = 0;
-        bucket_file.open(last_bucket);
+        bucket_file.open(bucket);
         if(bucket_file.is_open()){
-            cout << "Im open" << endl;
+            cout << "opened " << bucket << endl;
             while(getline(bucket_file,tmp))
                 bucket_counter++;
-            if (bucket_counter < fd){
+	    cout << "number of items in bucket: " << bucket_counter << "/" << fd << endl;
+            bucket_file.close();
+	    if (bucket_counter < fd){
                 //insertar aca
-                bucket_file.close();
-                ofstream newb;
-                newb.open(last_bucket, ios::app);
-                if(newb.is_open()){
-                    newb << input << endl;
-                    newb.close();
-                    cout << input << "im open again";
-                }
-                cout << "im closed again";
+                ofstream newbucket;
+                newbucket.open(bucket, ios::app);
+                if(newbucket.is_open()){
+		    cout << "opened " << bucket << endl;
+	    	    cout << "item to insert: " << input << endl;	    
+                    newbucket << input << endl;
+		}
+		newbucket.close();
+                cout << "closed " << bucket << endl;
             }
             else{
                 //crear nuevi bucket
-                ofstream new_bucket;
-                new_bucket.open(to_string(num_buckets).append(".txt"));
-                new_bucket << input << endl;
-                num_buckets++;
-                new_bucket.close();
+                ofstream newbucket;
+                newbucket.open(to_string(num_buckets).append(".txt"));
+                if(newbucket.is_open()){
+		    cout << "opened " << num_buckets << ".txt\n";
+		    newbucket << input << endl;
+		    ofstream index;
+		    index.open(to_string(bucket_id).append("index.txt"), ios::app);
+		    if(index.is_open())
+			index << to_string(num_buckets).append(".txt") << endl;
+		    num_buckets++;
+		    cout << "number of buckets: " << num_buckets << endl;
+		    index.close();
+		}
+                newbucket.close();
+		ofstream hash_file;
+	        hash_file.open("hash.txt");
+        	if(hash_file.is_open()){
+                    hash_file << to_string(HashTable_size) << endl;
+            	    hash_file << to_string(num_buckets) << endl;
+            	    cout << "updated number of buckets to " << num_buckets << endl;
+            	    hash_file << to_string(fd) << endl;
+        	}
+		hash_file.close();
+
             }
         }
-        else{cout << "im closed";}
-
-
-        /*
-        while(bucket->overflow_bucket){
-            bucket = bucket->overflow_bucket;
-        }
-
-        if(bucket_isfull(bucket)){
-            //Create new bucket
-            Bucket* tmp = new Bucket();
-            tmp->bucket_id = num_buckets;
-            tmp->bucket_name = to_string(num_buckets).append(".txt");
-
-            bucket->overflow_bucket = tmp;
-            num_buckets++;
-
-            bucket = tmp;
-        }
-
-        string name_of_txt = bucket->bucket_name;
-
-        ofstream output_file;
-        output_file.open(name_of_txt, ios::app);
-
-        if(output_file.is_open()){
-            output_file << input << endl;
-            bucket->counter += 1;
-        }
-
-        output_file.close();
-        */
         m.unlock();
     }
 
 
     string search(int key){
-        Bucket* bucket = &(hash_table[hash_function(key)]);
+        int bucket_id = hash_function(key);
+	ifstream index;
+	index.open(to_string(bucket_id).append("index.txt"));
+	if(index.is_open()){
+	    string bucket;
+            while(getline(index, bucket)){
+            	cout << bucket << endl;
+ 	        ifstream search_file;
+        	search_file.open(bucket);
 
-
-
-        while(true){
-            cout << bucket->bucket_name << endl;
-            string name_of_txt = bucket->bucket_name;
-            ifstream search_file;
-            search_file.open(name_of_txt);
-
-            string tmp;
-            stringstream line;
-            if(search_file.is_open()){
-                while(getline(search_file,tmp)){
-                    if(tmp.find(to_string(key)) != string::npos){
-                        return tmp;
+            	if(search_file.is_open()){
+		    string tmp;
+                    while(getline(search_file,tmp)){
+                    	if(tmp.find(to_string(key)) != string::npos){
+                            return tmp;
+                    	}
                     }
-                }
+            	}
+            	search_file.close();
             }
-            search_file.close();
-            if(bucket->overflow_bucket == nullptr)
-                return "not found";
-            else
-                bucket = bucket->overflow_bucket;
-        }
+	}
+	index.close();
+	return "404 Not Found";
     }
 
     bool is_file_exist(string fileName){
@@ -271,18 +237,6 @@ public:
         return infile.good();
     }
 
-
-    void print(){
-        for(int i = 0; i < HashTable_size; i++){
-            cout << (hash_table[i]).bucket_id << " " << (hash_table[i]).bucket_name;
-            Bucket *tmp = (hash_table[i]).overflow_bucket;
-            while(tmp){
-                cout << " -> " << tmp->bucket_id << " " << tmp->bucket_name;
-                tmp = tmp->overflow_bucket;
-            }
-            cout << endl;
-        }
-    }
 };
 
 #endif
